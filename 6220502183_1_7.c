9947fc41a58043d331b636e01359db6c5f090a36
saudrey <weerawutearth@gmail.com>
2021-02-04 17:30:49 +0700

    c

--- 6220502183_1_7.c
+++ 6220502183_1_7.c
@@ -1,18 +1,27 @@
 #include<stdio.h>
-
+#include<string.h>
 int main()
 {
     float time;
+    char z[];
 
     scanf ("%f",&time);
 
     if (time<12)
     {
-        printf("%.2f a.m.",time);
+         strcpy(z,"a.m.");
+        //printf("%.2f a.m.",time);
     }
     else
     {
-        printf("%.2f p.m.",time);
+        strcpy(z,"p.m.");
+        time=time-12;
+        //printf("%.2f p.m.",time);
     }
+    if (time==0)
+    {
+        time=12;
+    }
+    
     return 0;
 }
